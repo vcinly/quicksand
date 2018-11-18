8f1e46fe0d63306af40479507d608cf71f9783e4
Vcinly <vcinly@gmail.com>
2018-11-19 00:15:19 +0800

    support pgsql

--- sand.go
+++ sand.go
@@ -8,6 +8,10 @@ import (
 	"./address"
 	"crypto/ecdsa"
 	crand "crypto/rand"
+
+	"database/sql"
+
+	_ "github.com/lib/pq"
 )
 
 type Pair struct {
@@ -28,7 +32,7 @@ func newKey(rand io.Reader) (*Pair, error) {
 func newKeyFromECDSA(privateKeyECDSA *ecdsa.PrivateKey) *Pair {
 	pair := &Pair{
 		PrivateKey: address.EncodeToPrivateKey(privateKeyECDSA),
-		PublicKey:  address.EncodeToPublicKey(privateKeyECDSA),
+		// PublicKey:  address.EncodeToPublicKey(privateKeyECDSA),
 		BtcAddress: address.EncodeToBitcoin(privateKeyECDSA),
 		EthAddress: address.EncodeToEthereum(privateKeyECDSA),
 	}
@@ -37,11 +41,9 @@ func newKeyFromECDSA(privateKeyECDSA *ecdsa.PrivateKey) *Pair {
 
 var count = 0
 var now = time.Now()
-func main() {
 
+func test() {
 	fmt.Printf(now.Format("2006-01-02T15:04:05Z07:00\n"))
-
-
 	for {
 		_, err := newKey(crand.Reader)
 		if err != nil { fmt.Errorf("%v", err) }
@@ -53,6 +55,48 @@ func main() {
 			fmt.Println(count, "这是")
 			break
 		}
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+const (
+	host = "localhost"
+	port = 5432
+	user = "postgres"
+	dbname=""
+)
+
+
+func main() {
+	fmt.Printf(now.Format("2006-01-02T15:04:05Z07:00\n"))
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",host,port,user,dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	checkErr(err)
+	defer db.Close()
+
+	for {
+		pair, err := newKey(crand.Reader)
+		checkErr(err)
+		stmt, err := db.Prepare("INSERT INTO sand(private_key,btc_address,eth_address) VALUES($1,$2,$3)")
+		checkErr(err)
+		_, err = stmt.Exec(pair.PrivateKey, pair.BtcAddress, pair.EthAddress)
+		checkErr(err)
+		break
+
+		//count ++
+
+		//time.Now().Minute()
+		//if time.Now().Second() > now.Second() {
+		//	fmt.Println(now.Second(), "now")
+		//	fmt.Println(time.Now().Second(), "Now")
+		//	fmt.Println(count, "这是")
+		//	break
+		//}
 	}
 
 
